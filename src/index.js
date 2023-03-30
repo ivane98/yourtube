6bf21a155b3a74ebd2d4f66293cb94f48d0ac4b8
ivane98 <i.vardoshvili22@gmail.com>
2023-03-30 16:53:26 +0400

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+//// "proxy": "http://localhost:3001/api",
 
 if (process.env.NODE_ENV === "production") disableReactDevTools();
 
